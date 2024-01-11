8908becd8ea889ac80d85cb10f7bb51157d6efc8
Eduardo Misael <eduardomisaell@gmail.com>
2024-01-11 20:28:04 -0300

    Fixing DateUtils tests

--- src/test/java/br/ce/wcaquino/taskbackend/utils/DateUtilsTest.java
+++ src/test/java/br/ce/wcaquino/taskbackend/utils/DateUtilsTest.java
@@ -8,17 +8,17 @@ import java.time.LocalDate;
 public class DateUtilsTest {
     @Test
     public void testShouldReturnTrueForFutureDate(){
-        LocalDate localDate = LocalDate.of(2025,01,01);
+        LocalDate localDate = LocalDate.now().plusDays(1);
         Assert.assertTrue(DateUtils.isEqualOrFutureDate(localDate));
     }
     @Test
     public void testShouldReturnTrueForPastDate(){
-        LocalDate localDate = LocalDate.now();
+        LocalDate localDate = LocalDate.now().minusDays(1);
         Assert.assertTrue(DateUtils.isEqualOrFutureDate(localDate));
     }
     @Test
     public void testShouldReturnTrueForTheCurrentDate(){
-        LocalDate localDate = LocalDate.of(2023,01,01);
+        LocalDate localDate = LocalDate.now();
         Assert.assertFalse(DateUtils.isEqualOrFutureDate(localDate));
     }
 
